e4ce17b9fa76feb12a7d7b06d241491ad685fd87
OstapTelychko <telychko1916@gmail.com>
2025-12-06 16:17:23 +0100

    minor fix

--- AppManagement/search.py
+++ AppManagement/search.py
@@ -6,7 +6,6 @@ from AppObjects.app_core import AppCore
 def show_search_window() -> None:
     """Show search window."""
 
-
     categories = list(AppCore.instance().categories.values())
     WindowsRegistry.SearchWindow.categories_selection.add_categories_to_selection(categories)
     WindowsRegistry.SearchWindow.exec()
